26e865b4a0cf872feed8ce34acadc6faab96bf79
JackieCalapristi <jackie.lc11@gmail.com>
2019-04-20 16:12:13 -0700

    Adding almost everything that beyonce has done in her life

--- src/LifeEvents.js
+++ src/LifeEvents.js
@@ -7,72 +7,148 @@ var futureDay = new Date(Date.now() + 1000 * 60 * 60 * 24 * 10 * 6)
 
 export default {
   relationships: [
-    { name: "Cole", start: "2016-01-30", end: "2018-10-15", color: "pink" },
-    {
-      name: "Single Adventures", start: "2013-09-21", end: "2015-05-30",
-      color: "pink"
-    },
-    { name: "Will", start: "2015-06-01", end: "2015-11-20", color: "pink" },
-    { name: "Jake", start: "2008-11-20", end: "2013-09-20", color: "pink" }
-  ],
-  today: [{ name: ".", start: today, end: today, color: "#90eea8" }],
-  firstProgrammingClass: [
-    {
-      name: "First Programming Class",
-      start: "2011-09-01",
-      end: "2011-09-01",
-      color: "#90eea8"
-    }
+    { name: "Jay-Z", start: "2001-11-01", end: "present", color: "pink" },
   ],
   education: [
     {
-      name: "Elementary School",
+      name: "St. Mary’s Elementary School",
+      start: "1987-09-01",
+      end: "1990-06-12",
+      color: "#ADD8E6"
+    },
+    {
+      name: "Parker Elementary School",
+      start: "1990-09-01",
+      end: "1994-06-12",
+      color: "#ADD8E6"
+    },
+    {
+      name: "Kinder High School for the Performing and Visual Arts",
+      start: "1994-09-01",
+      end: "1996-06-12",
+      color: "#ADD8E6"
+    },
+    {
+      name: "Alief Elsik High School",
       start: "1996-09-01",
-      end: "2002-06-12",
+      end: "1998-06-12",
       color: "#ADD8E6"
     },
-    {
-      name: "Middle School",
-      start: "2002-09-01",
-      end: "2005-06-12",
-      color: "#ADD8E6"
-    },
-    {
-      name: "High School",
-      start: "2005-09-01",
-      end: "2009-06-12",
-      color: "#ADD8E6"
-    },
-    {
-      name: "University",
-      start: "2009-09-01",
-      end: "2013-06-11",
-      color: "#ADD8E6"
-    }
   ],
-  // retirement: [
-  //   { name: "Planned Date", start: "", end: "none" } // For milestones use end: "none"
-  // ],
+  kids: [
+    {
+      name: "Blue Ivy Carter",
+      start: "2012-01-07",
+      end: "present",
+      color: "#e5adef"
+    },
+    {
+      name: "Rumi Carter",
+      start: "2017-06-13",
+      end: "present",
+      color: "#e5adef"
+    },
+    {
+      name: "Sir Carter",
+      start: "2017-06-13",
+      end: "present",
+      color: "#e5adef"
+    },
+  ],
   jobs: [
     {
-      name: "DOE Internship",
-      start: "2012-06-10",
-      end: "2012-09-10",
+      name: "Owner of Ivy Park",
+      start: "2016-03-31",
+      end: "present",
+      color: "#f9d849"
+    }
+  ],
+  groups: [
+    {
+      name: "Destiny's Child",
+      start: "1996-01-01",
+      end: "2006-01-01",
       color: "orange"
     },
     {
-      name: "POS-X Internship",
-      start: "2013-04-01",
-      end: "2013-06-10",
+      name: "Solo Artist",
+      start: "2006-01-01",
+      end: "present",
       color: "orange"
     },
-    { name: "Vitech", start: "2013-07-01", end: "2015-03-02", color: "orange" },
     {
-      name: "Conversica",
-      start: "2015-04-20",
-      end: "2017-11-10",
+      name: "The Carters",
+      start: "2018-06-16",
+      end: "present",
       color: "orange"
     },
-    { name: "10Kft", start: "2017-11-27", end: "2019-02-22", color: "orange" }
+  ],
+  albums: [
+    {
+      name: "Destiny's Child",
+      start: "1998-02-17",
+      end: "1999-07-26",
+      color: "#90eea8"
+    },
+    {
+      name: "The Writing's On The Wall",
+      start: "1999-07-27",
+      end: "2001-04-30",
+      color: "#90eea8"
+    },
+    {
+      name: "Survivor",
+      start: "2001-05-01",
+      end: "2001-10-29",
+      color: "#90eea8"
+    },
+    {
+      name: "8 Days of Christmas",
+      start: "2001-10-30",
+      end: "2004-11-15",
+      color: "#90eea8"
+    },
+    {
+      name: "Destiny Fulfilled",
+      start: "2004-11-16",
+      end: "2006-08-31",
+      color: "#90eea8"
+    },
+    {
+      name: "Dangerously In Love",
+      start: "2003-06-23",
+      end: "2006-08-31",
+      color: "#90eea8"
+    },
+    {
+      name: "B'Day",
+      start: "2006-09-01",
+      end: "2008-11-11",
+      color: "#90eea8"
+    },
+    {
+      name: "I Am... Sasha Fierce",
+      start: "2008-11-12",
+      end: "2011-06-23",
+      color: "#90eea8"
+    },
+    {
+      name: "4",
+      start: "2011-06-24",
+      end: "2013-12-12",
+      color: "#90eea8"
+    },
+    {
+      name: "Beyoncé",
+      start: "2013-12-13",
+      end: "2016-04-22",
+      color: "#90eea8"
+    },
+    {
+      name: "Lemonade",
+      start: "2016-04-23",
+      end: "present",
+      color: "#90eea8"
+    },
   ]
 };
--- src/index.js
+++ src/index.js
@@ -19,9 +19,11 @@ const App = ({ birthdate }) => (
       <Headers birthdate={birthdate} />
       <div style={{ display: "flex" }}>
         <Key color="pink" title="Relationships" />
-        <Key color="orange" title="Jobs" />
         <Key color="#ADD8E6" title="Education" />
-        <Key color="#90eea8" title="Today" />
+        <Key color="orange" title="Groups" />
+        <Key color="#90eea8" title="Albums" />
+        <Key color="#f9d849" title="Companies" />
+        <Key color="#e5adef" title="Kids" />
       </div>
     <div style={{ display: 'flex' }}>
       {/* <div style={{ width: '200px' }}> FUTURE SIDE MENU </div> */}
@@ -59,4 +61,4 @@ const App = ({ birthdate }) => (
   </div>
 );
 
-render(<App birthdate="1991-06-12" />, document.getElementById("root"));
+render(<App birthdate="1981-09-04" />, document.getElementById("root"));
